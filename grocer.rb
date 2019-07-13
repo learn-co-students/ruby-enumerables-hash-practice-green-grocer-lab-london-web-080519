73158cfcf533c11781bc4ae7da66327cee24b66c
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-13 14:31:14 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,18 @@
 def consolidate_cart(cart)
   # code here
+  cart_consolidated = {}
+
+  cart.each do |element|
+    if cart_consolidated.include?(element[0])
+      cart_consolidated[element[0]][:count] += 1
+    else
+      cart_consolidated = {
+        element[0] = {
+          price: element[1][:price],
+          clearance: element[1][:clearance],
+          count: 1
+        }
+      }
 end
 
 def apply_coupons(cart, coupons)
@@ -12,4 +25,5 @@ end
 
 def checkout(cart, coupons)
   # code here
+
 end
